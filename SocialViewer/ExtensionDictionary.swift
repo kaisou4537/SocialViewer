f18f6e3efc628682216325793fb4e34a79a1039a
kaisou <kaisou4537@gmail.com>
2014-10-05 23:57:16 +0900

    add extension

---
+++ SocialViewer/ExtensionDictionary.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionDictionary.swift
+//  SocialViewer
+//
+//  Created by 佐々木 健 on 2014/10/05.
+//  Copyright (c) 2014年 ssk. All rights reserved.
+//
+
+import Foundation
---
+++ SocialViewer/OauthTwitterClient.swift
@@ -0,0 +1,9 @@
+//
+//  OauthTwitterClient.swift
+//  SocialViewer
+//
+//  Created by 佐々木 健 on 2014/10/05.
+//  Copyright (c) 2014年 ssk. All rights reserved.
+//
+
+import Foundation
